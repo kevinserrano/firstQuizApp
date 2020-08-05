106adf8b22b86f3f3bb050da516aa56c5f06e9f6
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-05 17:42:19 -0500

    started coding quiz app

---
+++ script.js
@@ -0,0 +1,13 @@
+var generateBtn = document.querySelector(".btn-primary");
+generateBtn.addEventListener("click", strtQuiz);
+
+funtion strtQuiz() {
+
+    var qzQuestion = generateQuestion();
+    var qzAns = ;
+    var userResult = ;
+}
+
+function generateQuestion() {
+
+}
