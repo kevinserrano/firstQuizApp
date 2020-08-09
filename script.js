56c594ad45b7b4b39d0f170d6ad3e0f850c22f78
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-09 11:22:07 -0500

    added array holding questions and answers for the quiz

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var generateBtn = document.querySelector(".btn-primary");
 generateBtn.addEventListener("click", strtQuiz);
 
+
+
+//make vars for questions, answers, correct, wrong, final score
 //start quiz
 //screen change and 1st question with answers is generated
 //allow user to pick answer
@@ -10,9 +13,25 @@ generateBtn.addEventListener("click", strtQuiz);
 
 
 
-var qzQuestion = ["What does DRY in the coding world mean?", "What does the acronym MERN stand for?", ""]
-var qzAns;
-var userResult;
+var qzQuestion = [{
+    question: " What do if and else statements always evaluate to?",
+    questionOptions: "",
+    answer: 1,
+}, {
+    question: " What are the 3 building blocks of the internet?",
+    questionOptions: "",
+    answer: 1,
+}, {
+    question: " What type of language is JavaScript?",
+    questionOptions: "",
+    answer: 1,
+}, {
+    question: " What does var do?",
+    questionOptions: "",
+    answer: 1,
+}]
+
+
 
 
 
