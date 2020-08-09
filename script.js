9e535ea9d0799d1cb134756b9c7eb1b57cbb35a6
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-08-09 10:56:25 -0500

    added psuedo notes in java and some variables. also removed some css to start over

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 var generateBtn = document.querySelector(".btn-primary");
 generateBtn.addEventListener("click", strtQuiz);
 
+//start quiz
+//screen change and 1st question with answers is generated
+//allow user to pick answer
+//after submit is pushed next question comes up
+// when quiz is over promps the users score and a place for them to enter initials
+
+
+
+
+var qzQuestion = ["What does DRY in the coding world mean?", "What does the acronym MERN stand for?", ""]
+var qzAns;
+var userResult;
+
+
+
+
 funtion strtQuiz() {
 
     var qzQuestion = generateQuestion();
-    var qzAns = ;
-    var userResult = ;
+    var qzAns;
+    var userResult;
 }
+
 
 function generateQuestion() {
 
